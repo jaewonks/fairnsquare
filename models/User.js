eb8e818d58f06ef4d9782a297d77947db334ada0
Jaewon Kim <jaewon@Jaewons-MBP.cust.communityfibre.co.uk>
2020-10-06 08:35:05 +0100

    commented

--- models/User.js
+++ models/User.js
@@ -1,5 +1,9 @@
-const mongoose = require("mongoose")
+const mongoose = require('mongoose')
 //몽구스를 이용해서 스키마를 생성한다. 
+const bcrypt = require('bcrypt')
+const saltRounds = 10 // 몇자리 솔트를 만드는가
+const jwt = require('jsonwebtoken')
+
 const userSchema = mongoose.Schema({
     name: {
         type: String, 
@@ -30,7 +34,91 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 })
+
+userSchema.pre('save', function( next ) {
+    const user = this;
+    
+    if(user.isModified('password')){    
+        console.log('password changed')
+        bcrypt.genSalt(saltRounds, function(err, salt){
+            if(err) return next(err);
+    
+            bcrypt.hash(user.password, salt, function(err, hash){
+                if(err) return next(err);
+                console.log(err)
+                user.password = hash 
+                next()
+            })
+        })
+    } else {
+        next()
+    }
+});
+
+/* user.save((err, userInfo) => { ... 이렇게 저장을 하기 전에 무언가를 하고 이리로 보낸다. 
+userSchema.pre('save',(next) => {
+    솔트를 생성하고 생성된 솔트로 암호회 시킨다.
+    비밀번호를 암호화시킨다.
+    const user = this;
+    if(user.isModified('password')){ 패스워드 부분을 수정할때만 암호화한다.
+        console.log('password changed')
+        bcrypt.genSalt(saltRounds, (err, salt) => {
+            if(err) return next(err) register라우터로 에러와 함께 보내준다.
+            //암호화되지 않은 비번은 onst user = new User(req.body)에 담겨저 schema로 전달됬으니 
+            bcrypt.hash(user.password, salt, (err, hash) => { //hash는 암호화된 비번
+                if(err) return next(err) register라우터로 에러와 함께 보내준다.
+                //암호화된 비번 만드는데 성공
+                user.password = hash
+                next() 메서드, 자기가 처리하지 못하는 것을 다음 매서드로 던저주는 기능
+                }) 
+            })
+        } else {
+            next() next가 있어야 빠져나간다. next가 없으면 이 안에 머문다.
+        }
+    })
+
+이메일이 있다면 비밀번호 일치 여부 확인 - User.js에서 메소드를 만든다.
+user.comparePassword(req.body.password, (err, isMatch) => {
+    if(!isMatch) return res.json({ loginSuccess: false, message:'Wrong password! please check it again' })
+}) */
+
+userSchema.methods.comparePassword = (rawPW, callback) => {
+    //암호화되지 않은 비번과 암호화된 비번 체크
+    bcrypt.compare(rawPW, this.password, (err, isMatch) => {
+        console.log(this.password)
+        if(err) return callback(err)
+        callback(null, isMatch) 
+        //에러는 없고 isMatch는 true
+    })
+}
+
+/* user.generateToken((err,user) => {
+    if(err) return res.status(400).send(err)
+    토큰을 저장한다. 어디에...?
+    res.cookie("tokenInCookie", user.token)
+    .status(200)
+    .json({ loginSuccess:true, userId: user._id })
+}) */
+
+userSchema.methods.generateToken = (callback) => {
+    const user = this;
+    //jsonwebtoken을 이용하여 token생성하기 몽고 DB 아이디(_id)
+    //user._id + 'userToken' = token
+    const token = jwt.sign(user._id.toHexString(),'userToken')
+
+    user.token = token
+    user.save((err, user) => { 
+        //userSchema에 저장
+        if(err) return callback(err)
+        callback(null, user) 
+        //에러는 없고 유저 정보만 전달.
+    })
+    
+}
+
+
 //스키마를 모델로 감싸준다.
 const User = mongoose.model('User', userSchema)
+
 //다른 곳에서도 사용 가능하도록 export
 module.exports = { User }
--- server.js
+++ server.js
@@ -1,14 +1,15 @@
-const { doesNotMatch, doesNotReject } = require('assert')
 const express = require('express')
 const app = express()
 const port = 5000
 const bodyParser = require('body-parser') //미들웨어 클라이언트에서 오는 정보를 분석해서 가진다.
+const cookieParser = require('cookie-parser')
 const mongoose = require('mongoose')
 const { User } = require('./models/User')
 const config = require('./config/key')
 
 app.use(bodyParser.urlencoded({extended: true})) //application/x-www=form-urlencoded에서 분석된 정보를 가져온다.
 app.use(bodyParser.json()) //application/json 타입으로 된 것을 분석해서 가져온다.
+app.use(cookieParser())
 
 mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
@@ -21,11 +22,36 @@ app.get('/', (req, res) => res.send('hello my first project'))
 app.post('/register', (req,res) => {
     //회원가입할때 필요한 정보들을 Client에서 가져오면 
     const user = new User(req.body) //클라이언트에서 보내온 정보를 담는다.
-    console.log(req.body)
+    //console.log(req.body)
     //그것들을 데이터베이스에 넣어준다 .save() 유저모델에 저장이 된 것
-    user.save((err, userInfo) => { //만약 에러가 있다면 클라이언트에 json방식으로 전달.
-        if(err) return res.json({ success: false, err }) //상태와 에러 메세지도 함께 전달
+    //..user모델에서 저장하기 전에 비밀번호를 암호화 하는 중
+    user.save((err, user) => { //만약 에러가 있다면 클라이언트에 json방식으로 전달.
+        if (err) return res.json({ success: false, err }); //상태와 에러 메세지도 함께 전달
         return res.status(200).json({ success: true })
+    })
+})
+app.post('/login', (req, res) => {
+    //데이터 베이스 안에서 요청된 이메일 찾기
+    User.findOne({ email: req.body.email }, (err,user) => {
+        if(!user) return res.json({
+            loginSuccess: false, message: 'Fail to login'
+        })
+  
+        //이메일이 있다면 비밀번호 일치 여부 확인 - User.js에서 메소드를 만든다.
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            console.log('err',err)
+            console.log('isMatch', isMatch)
+            if(!isMatch) return res.json({ loginSuccess: false, message:'Wrong password! please check it again' })
+        
+            //비밀번호까지 일치하면 토큰을 생성하기 - User.js에서 메소드를 만든다.
+            user.generateToken((err,user) => {
+                if(err) return res.status(400).send(err)
+                //토큰을 저장한다. 어디에...?
+                res.cookie("tokenInCookie", user.token)
+                .status(200)
+                .json({ loginSuccess:true, userId: user._id })
+            })
+        })
     })
 })
 
